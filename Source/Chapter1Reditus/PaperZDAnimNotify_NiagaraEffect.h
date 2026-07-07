f718b3053b23d83cd6293c2856043c6dfcde0278
Yashua Lebowitz <meshiachkingdom@gmail.com>
2026-07-07 16:37:57 +0530

    xvxvxv

---
+++ Source/Chapter1Reditus/PaperZDAnimNotify_NiagaraEffect.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Notifies/PaperZDAnimNotify_ParticleEffect.h"
+#include "PaperZDAnimNotify_NiagaraEffect.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHAPTER1REDITUS_API UPaperZDAnimNotify_NiagaraEffect : public UPaperZDAnimNotify_ParticleEffect
+{
+	GENERATED_BODY()
+	
+};
